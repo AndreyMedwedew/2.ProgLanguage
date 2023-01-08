6c56bb3c698a33e71e9ff7e4e62de4fb5d442ad5
Медведев Андрей <AndreyMedwedew3@gmail.com>
2023-01-08 13:40:04 +0300

    condition added

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
 Console.Clear();
 Console.WriteLine("Add a StartNumber:");
 int StartNumber = int.Parse(Console.ReadLine());
@@ -11,7 +15,6 @@ void SumNumbers(int m, int n)
     int sum = 0;
     int i = 0;
     for(i=m; i<n+1; i+=1) sum+=i;
-    // return sum;
     Console.Write($"SumNumbers from {m} to {n} is {sum}");
 }
 
