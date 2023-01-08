c4c1cb7490f6c988521bac4546518096570e433b
Медведев Андрей <AndreyMedwedew3@gmail.com>
2023-01-08 13:35:50 +0300

    task 68 completed

---
+++ Task68/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int Ackermann(int m, int n)
+{
+    if(m==0) return n + 1;
+    else if (m>0 && n == 0) return Ackermann(m-1, 1);
+    return Ackermann(m-1, Ackermann(m, n - 1));
+}
+
+Console.WriteLine(Ackermann(3, 2));
