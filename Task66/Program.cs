dca3013521219298b45d4cd4bdc9615e8f0f0d97
Медведев Андрей <AndreyMedwedew3@gmail.com>
2023-01-08 13:18:33 +0300

    task 66 completed

---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Clear();
+Console.WriteLine("Add a StartNumber:");
+int StartNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Add a FinishNumber:");
+int FinishNumber = int.Parse(Console.ReadLine());
+
+void SumNumbers(int m, int n)
+{
+    int sum = 0;
+    int i = 0;
+    for(i=m; i<n+1; i+=1) sum+=i;
+    // return sum;
+    Console.Write($"SumNumbers from {m} to {n} is {sum}");
+}
+
+SumNumbers(StartNumber, FinishNumber);
