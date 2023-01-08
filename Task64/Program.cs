fabe459780a8fa6b58cb9df2ccf20ea82b0ca57e
Медведев Андрей <AndreyMedwedew3@gmail.com>
2023-01-08 12:53:13 +0300

    task 64 completed

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// void LineNumbers(int n)
+// {
+//     if (n!=1) LineNumbers(n-1);
+//     Console.Write($"{n} ");
+// }
+
+Console.Clear();
+Console.WriteLine("Add a Number:");
+int number = int.Parse(Console.ReadLine());
+
+void LineNumbers(int n)
+{
+    if (n!=1) 
+    {
+        Console.Write($"{n}, ");
+        LineNumbers(n-1);
+    }
+    else Console.Write($"{n}");
+}
+
+LineNumbers(number);
