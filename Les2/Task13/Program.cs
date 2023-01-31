47c7dc45281ca9741edece9c0e94403c36a1c01b
Медведев Андрей <AndreyMedwedew3@gmail.com>
2023-01-31 22:04:01 +0300

    10, 13, 15 tasks completed

---
+++ Les2/Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+int N = int.Parse(Console.ReadLine());
+if (N<100)  Console.Write("number does not have a third digit");
+else 
+{
+while (N>=1000)
+{
+N /= 10;
+}
+N %= 10;
+Console.WriteLine($"a third digit is {N}");
+}
