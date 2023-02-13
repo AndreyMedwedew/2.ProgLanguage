99f8b4671e09bbc0e3015d2d0251a4d6028ed07a
Медведев Андрей <AndreyMedwedew3@gmail.com>
2023-02-13 21:54:26 +0300

    41, 43  tasks completed

---
+++ Les6/Task41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("how many numbers do you want to enter");
+int x1 = int.Parse(Console.ReadLine());
+
+int[] CreateArray(int size) 
+{
+    int[] array = new int[size];
+    for(int i=0;i<array.Length;i+=1) 
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array; 
+}
+
+void numbersgreaterthanzero (int[] array) 
+{
+    int count=0;
+    for(int i=0; i<array. Length;i+=1) 
+    {
+        if(array[i]>0) count += 1;
+    }
+    Console.Write(count); 
+}
+
+numbersgreaterthanzero(CreateArray(x1));
---
+++ Les6/Task43/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double b1 = double.Parse(Console.ReadLine());
+double k1 = double.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
+
+void xy(double b1, double k1, double b2, double k2) 
+{
+    double x = (b2-b1) / (k1-k2);
+    double y = k1*x+b1;
+    Console.Write($"{x}, {y}"); 
+}
+
+xy(b1, k1, b2, k2);
